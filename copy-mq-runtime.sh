837fbbce63967a03444a3db945481286ae82e629
Simon Godik <simon@godik.com>
2021-06-10 15:17:54 -0700

    mq install noi docker file and scripts

--- copy-mq-runtime.sh
+++ copy-mq-runtime.sh
@@ -1,70 +1,75 @@
 #!/bin/bash -xe
 
+noidir=$1
+installdir=$2
+
 # select mq packages for installation
 
-#askquestion inc32 "Does the runtime require 32-bit application support [Y/N]? " "" "" 32
+# inc32 "Does the runtime require 32-bit application support?"
 export genmqpkg_inc32=0
 
-#askquestion incnls "Does the runtime require support for languages other than English [Y/N]? " "" "Msg_cs Msg_de Msg_es Msg_fr Msg_hu Msg_it Msg_ja Msg_ko Msg_pl Msg_pt Msg_ru Msg_Zh_CN Msg_Zh_TW" nls
+# incnls "Does the runtime require support for languages other than English?"
 export genmqpkg_incnls=0
 
-#askquestion inccpp "Does the runtime require C++ libraries [Y/N]? " "" "" cpp
+# inccpp "Does the runtime require C++ libraries?"
 export genmqpkg_inccpp=0
 
-#askquestion inccbl "Does the runtime require COBOL libraries [Y/N]? " "" "" cobol
+# inccbl "Does the runtime require COBOL libraries?"
 export genmqpkg_inccbl=0
 
-#askquestion incdnet "Does the runtime require .NET libraries [Y/N]? " "" "" dotnet
+# incdnet "Does the runtime require .NET libraries?"
 export genmqpkg_incdnet=0
 
-#askquestion inctls "Does the runtime require SSL/TLS support [Y/N]? "
+# inctls "Does the runtime require SSL/TLS support?"
 export genmqpkg_inctls=1
 
-#askquestion incams "Does the runtime require AMS support [Y/N]? " "" AMS
+# incams "Does the runtime require AMS support?"
 export genmqpkg_incams=0
 
-#askquestion inccics "Does the runtime require CICS support [Y/N]? " "" "" cics
+# inccics "Does the runtime require CICS support [Y/N]?"
 export genmqpkg_inccics=0
 
-#askquestion incadm "Does the runtime require any administration tools [Y/N]? " "" "" adm
+# incadm "Does the runtime require any administration tools?"
 export genmqpkg_incadm=1
 
-#askquestion incras "Does the runtime require any RAS tools [Y/N]? " "" "" ras
+# incras "Does the runtime require any RAS tools?"
 export genmqpkg_incras=1
 
-#askquestion incsamp "Does the runtime require any sample applications [Y/N]? " "" Samples samp
+# incsamp "Does the runtime require any sample applications"
 export genmqpkg_incsamp=0
 
-#askquestion incsdk "Does the runtime require the SDK to compile applications [Y/N]? " "" SDK
+# incsdk "Does the runtime require the SDK to compile applications?"
 export genmqpkg_incsdk=1
 
-#askquestion incunthrd "Does the runtime require unthreaded application support [Y/N]? " "" "" unthrd
+# incunthrd "Does the runtime require unthreaded application support?"
 export genmqpkg_incunthrd=0
 
-#askquestion incjre "Does the runtime require a Java Runtime Environment (JRE) [Y/N]? " "$mqdir/java/jre64" JRE
+# incjre "Does the runtime require a Java Runtime Environment (JRE)"
 export genmqpkg_incjre=1
 
-#askquestion incamqp "Does the runtime require AMQP support [Y/N]? " "$mqdir/amqp" AMQP
+# incamqp "Does the runtime require AMQP support? "
 export genmqpkg_incamqp=0
 
-#askquestion incman "Does the runtime require man pages [Y/N]? " "$mqdir/man" Man
+# incman "Does the runtime require man pages?"
 export genmqpkg_incman=0
 
-#askquestion incmqbc "Does the runtime require the Bridge to blockchain [Y/N]? " "$mqdir/mqbc" BCBridge
+# incmqbc "Does the runtime require the Bridge to blockchain?"
 export genmqpkg_incmqbc=0
 
-#askquestion incmqft "Does the runtime require Managed File Transfer [Y/N]? " "$mqdir/mqft" "FTAgent FTBase FTLogger FTService FTTools" mft
+# incmqft "Does the runtime require Managed File Transfer?"
 export genmqpkg_incmqft=0
 
-#askquestion incmqsf "Does the runtime require the Bridge to Salesforce [Y/N]? " "$mqdir/mqsf" SFBridge
+# incmqsf "Does the runtime require the Bridge to Salesforce?"
 export genmqpkg_incmqsf=0
 
-#askquestion incmqxr "Does the runtime require Telemetry (MQXR) support [Y/N]? " "$mqdir/mqxr" XRService
+# incmqxr "Does the runtime require Telemetry (MQXR) support?"
 export genmqpkg_incmqxr=1
 
-#askquestion incweb "Does the runtime require the MQ Console [Y/N]? " "$mqdir/web" Web
+# incweb "Does the runtime require the MQ Console?"
 export genmqpkg_incweb=1
 
+#
 #install mq
-# wget https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqadv/9.2.2.0-IBM-MQ-Advanced-for-Developers-Non-Install-LinuxX64.tar.gz
-/Users/simon/txmq/mq/non-install/bin/genmqpkg.sh -b /tmp/genmqpkg
+#
+
+$noidir/bin/genmqpkg.sh -b $installdir
---
+++ mq-install-noi.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -xe
+
+# copy mq runtime to install dir
+noidir=$1
+installdir=$2
+uid=$3
+
+./copy-mq-runtime.sh $noidir $installdir
+
+# change installdir ownership
+chown -R $uid:root $installdir/*
+
+# accept license
+$installdir/bin/mqlicense -accept
+
+# create mout points
+install -d -m 2775 -o $uid -g root /mnt
+install -d -m 2775 -o $uid -g root /mnt/mqm
+install -d -m 2775 -o $uid -g root /mnt/mqm/data
+install -d -m 2775 -o $uid -g root /mnt/mqm-log
+install -d -m 2775 -o $uid -g root /mnt/mqm-log/log
+install -d -m 2775 -o $uid -g root /mnt/mqm-data
+install -d -m 2775 -o $uid -g root /mnt/mqm-data/qmgrs
+
+# mq configuration files
+install -d -m 2775 -o $uid -g root /etc/mqm
+
+# directory for mq runtime files
+install -d -m 2755 -o $uid -g root /run/runmqserver
+
+touch /run/termination-log && chown $uid:root /run/termination-log
+
+# link /var/mqm to /mnt/mqm/data
+ln -s /mnt/mqm/data /var/mqm
