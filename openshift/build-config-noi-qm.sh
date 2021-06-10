70b0af7612e0d98d94070906e219ddd7d28e8f94
Simon Godik <simon@godik.com>
2021-06-10 15:30:59 -0700

    openshift noi qm build config

---
+++ openshift/build-config-noi-qm.sh
@@ -0,0 +1,24 @@
+apiVersion: build.openshift.io/v1
+kind: BuildConfig
+metadata:
+  name: mq-server-922
+  namespace: mqmq
+spec:
+  source:
+    git:
+      uri: 'https://github.com/szesto/mq-operator.git'
+    type: Git
+    images:
+    - from:
+        kind: DockerImage
+        name: "image-registry.openshift-image-registry.svc:5000/mqmq/mq-noi-tar:0.1"
+      paths:
+      - destinationDir: .
+        sourcePath: /tmp/MQServer-noi
+  strategy:
+    dockerStrategy:
+      dockerfilePath: Dockerfile-noi
+  output:
+    to:
+      kind: DockerImage
+      name: "image-registry.openshift-image-registry.svc:5000/mqmq/mq-server-noi:0.1"
