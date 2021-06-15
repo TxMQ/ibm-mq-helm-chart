75c57d85b4e39b38bc4089cd5815db2913dd3a93
Simon Godik <simon@godik.com>
2021-06-14 17:14:15 -0700

    working update create-start-wait-qm.sh

--- create-start-wait-qm.sh
+++ create-start-wait-qm.sh
@@ -1,16 +1,5 @@
 #!/bin/bash -x
 
-# create mq directories
-/opt/mqm/bin/crtmqdir -f -a
-
-# create queue manager
-/opt/mqm/bin/crtmqm -c "qm" -p 1414 -u SYSTEM.DEAD.LETTER.QUEUE -q qm
-
-# start queue manager
-/opt/mqm/bin/strmqm qm
-
-#pid=`ps -ef | grep qm | grep amqzxma0 | tr -s " " | cut -d " " -f 2`
-#ps -ef | grep $pid
-
+# sleep 10 days...
 sleep 10d
 
