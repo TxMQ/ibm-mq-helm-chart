fd5ed759a796e8a070c153ae81ae4e25ef803124
Simon Godik <simon@godik.com>
2021-08-19 22:31:47 -0700

    copy-certs tls gen directory argument

--- compose/copy-certs.sh
+++ compose/copy-certs.sh
@@ -2,6 +2,11 @@
 
 tlsdir=$1
 
+if [[ -z $tlsdir ]]; then
+echo tls directory required: copy-certs.sh '<tls-gen-dir>'. Use tls-gen to create keys and certs and point to the result directory.
+exit 1
+fi
+
 cp $tlsdir/server_key_nopass.pem output/etc/mqm/pki/cert/tls.key
 cp $tlsdir/server_certificate.pem output/etc/mqm/pki/cert/tls.crt
 cp $tlsdir/ca_certificate.pem output/etc/mqm/pki/cert/ca.crt
