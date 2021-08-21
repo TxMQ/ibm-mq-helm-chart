e992f7d6d7e3965c22371b60d3f5ba6d1873ccfb
Simon Godik <simon@godik.com>
2021-08-20 17:20:04 -0700

    podman docker-compose prereq

---
+++ prereq/podman-docker-compose.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+
+# start podman socket
+sudo systemctl start podman.socket
+
+# we should get OK
+sudo curl -H "Content-Type: application/json" --unix-socket /var/run/docker.sock http://localhost/_ping
