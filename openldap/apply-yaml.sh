16e2d0a887054d70955cb4ddd613a7cfa7172efb
Simon Godik <simon@godik.com>
2021-09-16 21:48:53 -0700

    ldif config map name for bitnami openldap

--- openldap/apply-yaml.sh
+++ openldap/apply-yaml.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [[ ! -f output/ldap-config-map.yaml ]]; then
-echo Run \'./ldif-template.sh\' script to create \'output/ldap-config-map.yaml\' file.
+echo Run \'./ldif-template.sh\' script to create \'output/ldif-config-map.yaml\' file.
 exit 1
 fi
 
 set -x
 
-kubectl apply -f output/ldap-config-map.yaml
+kubectl apply -f output/ldif-config-map.yaml
 kubectl apply -f ./bitnami-sa.yaml
 kubectl apply -f ./bitnami-service.yaml
 kubectl apply -f ./bitnami-deployment.yaml
