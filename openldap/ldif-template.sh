24d5e8a437a08a8743a31941d5a34b18a28a11bc
Simon Godik <simon@godik.com>
2021-09-16 21:21:07 -0700

    bitnami openldap ldif update

--- openldap/ldif-template.sh
+++ openldap/ldif-template.sh
@@ -62,10 +62,66 @@ data:
     sn: user3
     userPassword: $userpassword
 
+    dn: uid=user4,ou=users,$ldaproot
+    objectClass: inetOrgPerson
+    uid: user4
+    cn: user4
+    sn: user4
+    userPassword: $userpassword
+
+    dn: uid=user5,ou=users,$ldaproot
+    objectClass: inetOrgPerson
+    uid: user5
+    cn: user5
+    sn: user5
+    userPassword: $userpassword
+
+    dn: uid=user6,ou=users,$ldaproot
+    objectClass: inetOrgPerson
+    uid: user6
+    cn: user6
+    sn: user6
+    userPassword: $userpassword
+
+    dn: uid=user7,ou=users,$ldaproot
+    objectClass: inetOrgPerson
+    uid: user7
+    cn: user7
+    sn: user7
+    userPassword: $userpassword
+
+    dn: uid=user8,ou=users,$ldaproot
+    objectClass: inetOrgPerson
+    uid: user8
+    cn: user8
+    sn: user8
+    userPassword: $userpassword
+
+    dn: uid=user9,ou=users,$ldaproot
+    objectClass: inetOrgPerson
+    uid: user9
+    cn: user9
+    sn: user9
+    userPassword: $userpassword
+
     dn: cn=devs,ou=groups,$ldaproot
     objectClass: groupOfNames
     cn: devs
     member: uid=user1,ou=users,$ldaproot
     member: uid=user2,ou=users,$ldaproot
     member: uid=user3,ou=users,$ldaproot
+    member: uid=user4,ou=users,$ldaproot
+
+    dn: cn=admins,ou=groups,$ldaproot
+    objectClass: groupOfNames
+    cn: admins
+    member: uid=user5,ou=users,$ldaproot
+    member: uid=user6,ou=users,$ldaproot
+    member: uid=user7,ou=users,$ldaproot
+
+    dn: cn=readadmins,ou=groups,$ldaproot
+    objectClass: groupOfNames
+    cn: readadmins
+    member: uid=user8,ou=users,$ldaproot
+    member: uid=user9,ou=users,$ldaproot
 EOF
